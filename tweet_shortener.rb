8d3b8449a61802484189d03b72e6d4ff71850f49
Nathan Bell <nathanbell09@gmail.com>
2018-11-09 17:50:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def dictionary
-  words_to_be_subbed = {
+  {
     "hello" => "hi",
     "to, two, too" => "2",
     "for, four" => "4",
@@ -9,4 +9,8 @@ def dictionary
     "at" => "@",
     "and" => "&"
   }
+end
+
+def word_substituter(tweet)
+  
 end
