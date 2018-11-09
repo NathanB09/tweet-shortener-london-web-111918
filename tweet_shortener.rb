104d95ce89af9fc16f147caa433d60928d854d0c
Nathan Bell <nathanbell09@gmail.com>
2018-11-09 18:23:22 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,3 +31,7 @@ end
 def bulk_tweet_shortener(tweets)
   tweets.each { |tweet| puts word_substituter(tweet) }
 end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
