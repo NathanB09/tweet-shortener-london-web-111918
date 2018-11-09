66c18aff0ee83c05cc454f84d9a00a34e712cb70
Nathan Bell <nathanbell09@gmail.com>
2018-11-09 17:57:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,5 +12,13 @@ def dictionary
 end
 
 def word_substituter(tweet)
+  tweet_array = tweet.split
   
+  dictionary.each do |word, subtitute|
+    if tweet_array.include?(word)
+      tweet_array[tweet_array.index(word)] = subtitute
+    end
+  end
+
+  tweet_array.join
 end
